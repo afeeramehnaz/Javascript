c83695d7f29559eb1694ab653582812c7bc10924
Afeera Mehnaz <afeera.rc@gmail.com>
2024-01-02 08:23:00 +0530

    Js intro

---
+++ 02Variables/variables.js
@@ -0,0 +1,16 @@
+console.log("Variables in Javascript!!!");
+
+var a=100;
+console.log("value of a:",a);//100
+
+var b="Afeera";
+console.log("value of b:",b);//Afeera
+
+var c=null;
+console.log("value of c:",c);//null
+
+var d="";
+console.log("value of d:",d);//""
+
+console.log("value of e:",e);//variables.js:15 Uncaught ReferenceError: e is not defined
+
